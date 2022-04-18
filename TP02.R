2951c3ea5feba4fa5152bb6d4a6038cd8de28ebb
olivia-siegal <91622352+olivia-siegal@users.noreply.github.com>
2022-04-18 16:12:42 -0400

    Update TP02.R

--- TP02.R
+++ TP02.R
@@ -1,7 +1,10 @@
 ##### TP02: Analysis #####
+# Team 5: Mengting Ding, Dylan Koury, Olivia Siegal
 # Due Tuesday, April 19
 
 rm(list=ls())
+
+# No scientific notation
 options(scipen = 10000)
 
 ##### Libraries #####
@@ -17,12 +20,17 @@ library(MASS)
 library(naniar)
 library(glmnet)
 library(pls)
+library(randomForest)
+library(gbm)
+# install.packages("xgboost")
+library(xgboost)
 
 ##### Import data #####
+# available on Porto Seguro Competition kaggle page
 train = read.csv("train.csv",  row.names="id")
-test = read.csv("test.csv", row.names = "id")
 
 ##### Normalized Gini Coefficient Function #####
+# Code provided through kaggle to calculate normalized gini coefficient
 normalizedGini <- function(aa, pp) {
   Gini <- function(a, p) {
     if (length(a) !=  length(p)) stop("Actual and Predicted need to be equal lengths!")
@@ -30,27 +38,24 @@ normalizedGini <- function(aa, pp) {
     temp.df <- temp.df[order(-temp.df$pred, temp.df$range),]
     population.delta <- 1 / length(a)
     total.losses <- sum(a)
-    null.losses <- rep(population.delta, length(a)) # Hopefully is similar to accumulatedPopulationPercentageSum
-    accum.losses <- temp.df$actual / total.losses # Hopefully is similar to accumulatedLossPercentageSum
-    gini.sum <- cumsum(accum.losses - null.losses) # Not sure if this is having the same effect or not
+    null.losses <- rep(population.delta, length(a)) 
+    accum.losses <- temp.df$actual / total.losses
+    gini.sum <- cumsum(accum.losses - null.losses)
     sum(gini.sum) / length(a)
   }
   Gini(aa,pp) / Gini(aa,aa)
 }
 
-normalizedGini2 <- function(aa, pp) {
-  gini <- function(aa, pp) {
-    rank_pp <- data.table::frank(pp)
-    return(cov(rank_pp / mean(rank_pp), aa / mean(aa)))
-  }
-  return(gini(aa, pp) / gini(aa, aa))}
-
 
 ##### Explore the training dataset #####
+# Some of our models require that the y variable be a factor
 train$target = as.factor(train$target)
+
 head(train)
-str(train)
+str(train) # all features are numeric or integer
 summary(train)
+
+##### Missing Values #####
 # In this dataset, -1 represents missing data
 # Need to change -1 values to NA
 
@@ -65,16 +70,14 @@ replaceWNAs = function(x){
 }
 
 train = replaceWNAs(train)
-summary(train$ps_car_11)
+summary(train$ps_car_11) # to confirm all instances of -1 were changed to NA
 
 # Upon further exploration later in the analysis, we found that there were issues of multicollinearity with
 # ps_ind_14, ps_ind_09_bin so removing them here.
-head(train)
 train = train[,-c(10,15)]
 head(train)
 
 # Also need to remove NA's from dataset for complete analysis
-summary(train)
 # ps_reg_03, ps_car_03_cat, ps_car_05_cat, and ps_car_14 are comprised of mostly missing data, so drop these columns
 train = train[,-c(20,23,25,35)]
 summary(train)
@@ -95,23 +98,6 @@ replaceW0s = function(x){
 train = replaceW0s(train)
 summary(train)
 
-str(test)
-# Note, test.csv does not contain a target column
-# This data set is only provided to predict the target values
-# For training & testing purposes, we must split the train.csv dataset into a training and testing set
-
-# Replace -1 with NA's in the test.csv dataset
-test = replaceWNAs(test)
-summary(test)
-
-# Remove the same columns that were removed in train.csv
-test = test[,-c(9,14,21,24,26,28,36)]
-summary(test)
-
-# Change remaining NA's to zero
-test = replaceW0s(test)
-summary(test)
-
 ##### Split train.csv into training & testing #####
 set.seed(521)
 divideData = createDataPartition(train$target,p = 0.7, list=FALSE)
@@ -124,6 +110,7 @@ summary(train2$target)
 #      0      1 
 # 401463  15186 
 
+# Plot frequency of each class
 ggplot(data = train2, aes(fill = target)) +
   geom_bar(aes(x = target)) +
   ggtitle("Number of samples in each class", subtitle = "Training dataset") +
@@ -149,7 +136,7 @@ ggplot(data = train2, aes(fill = target)) +
 # Dataset - anonymized so difficult to make informed decisions about which features to include/consider
 # Did others center/scale the dataset?
 # Competitors say KNN didn't work well
-# multicollinearity considerations in the data? - avoid QDA b/c we 
+# multicollinearity considerations in the data?
 # set seeds? for reproducability
 
 # Models under consideration:
@@ -161,12 +148,13 @@ predictorVariables = train2[,-1]
 responseVariable = train2[,1]
 
 # For this function, the minority case must be 1 (true for this dataset)
-# Undersampling
+# Undersampling the majority class
 undersampledData = ubBalance(predictorVariables,responseVariable,type='ubUnder',verbose=TRUE)
 underTrain = cbind(undersampledData$Y,undersampledData$X)
 names(underTrain)[names(underTrain) == "undersampledData$Y"] <- "target"
 head(underTrain)
 
+# Plot the new fequencies
 ggplot(data = underTrain, aes(fill = target)) +
   geom_bar(aes(x = target)) +
   ggtitle("Number of samples in each class", subtitle = "Training dataset") +
@@ -184,7 +172,14 @@ summary(underTrain$target)
 # 0     1 
 # 15186 15186 
 
+# Classes are balanced
+
 ##### Logistic regression - unbalanced #####
+# Attempt to model the unbalanced dataset with the logistic regression
+# Just to better understand how things change when the classes are balanced
+
+# Preprocess the data
+# This preprocessing step is specific to the unbalanced dataset
 preprocessing2 <- train2 %>% preProcess(method = c("center", "scale"))
 trainTransformed2 <- preprocessing2 %>% predict(train2)
 testTransformed2 <- preprocessing2 %>% predict(validate)
@@ -192,21 +187,27 @@ testTransformed2 <- preprocessing2 %>% predict(validate)
 Logistic = glm(target~.,data=trainTransformed2, family = 'binomial')
 summary(Logistic)
 
+# Save the predicted probabilities to a vector
 LogisticProb = predict(Logistic,newdata=validate,type="response")
+
+# Assign predictions to class based on probability
 LogisticPred = ifelse(LogisticProb > 0.5,1,0) # Positive class is filing a claim (1)
 
-##Produce Accuracy rates 
+# Produce Accuracy rates 
 mean(LogisticPred==validate$target) # Accuracy rate of 0.96
+
+# Confusion Matrix
 table(LogisticPred,validate$target) # Only predicts 0 - lost of false positives
 
-# Not working
 normalizedGini(as.numeric(validate$target),as.numeric(LogisticPred)) # 0.005
-NormalizedGini(as.numeric(LogisticPred),as.numeric(validate$target)) # 0.005
 
 # Unbalanced dataset does not yield good models because the model assumes most of the
 # assignments should be 0. Accuracy is high, but won't be good at predicting.
 
 ##### Logistic Regression #####
+# Preprocess the balanced dataset
+# Center & Scaled data will be used for Logistic, LDA, QDA, and KNN models
+set.seed(521)
 preprocessing <- underTrain %>% preProcess(method = c("center", "scale"))
 trainTransformed <- preprocessing %>% predict(underTrain)
 testTransformed <- preprocessing %>% predict(validate)
@@ -214,26 +215,33 @@ testTransformed <- preprocessing %>% predict(validate)
 Logistic = glm(target~.,data=trainTransformed, family = 'binomial')
 summary(Logistic)
 
-car::vif(Logistic) # Two or more of the predictor variables are correlated
+# Logistic model assumption is that there is no multicollinearity
+# When we ran this model the first time, we observed multicollinearity between two features
+car::vif(Logistic)
+
+# Used the correlation matrix to identify the collinear features & removed them (see earlier code)
 cor(underTrain[,-1])
 # ps_ind_12_bin and ps_ind_14 have a correlation value of 0.8962608
 # ps_ind_09_bin included NA's in the summary of the Logistic model
 # removed ps_ind_09_bin and ps_ind_12_bin
 
+# Predict probabilities
 LogisticProb = predict(Logistic,newdata=validate,type="response")
+
+# Assign to class based on probability
 LogisticPred = ifelse(LogisticProb > 0.5,0,1) # Positive class is filing a claim (1)
 
-##Produce Accuracy rates 
+# Produce Accuracy rates 
 mean(LogisticPred==validate$target) # Accuracy rate of 0.7898053
+
+# Confusion Matrix
 table(LogisticPred,validate$target) # Lots of false negatives
 
-# Not working
-normalizedGini(as.numeric(validate$target),as.numeric(LogisticPred)) # -0.1690838
-NormalizedGini(as.numeric(LogisticPred),as.numeric(validate$target)) # -0.1690838
-
-normalizedGini2(as.numeric(validate$target),as.numeric(LogisticPred))
+# Calculate the normalized gini coefficient
+normalizedGini(as.numeric(validate$target),as.numeric(LogisticPred)) # -0.04246757
 
 ##### LDA #####
+set.seed(521)
 LDA = lda(target~.,data=trainTransformed)
 summary(LDA)
 plot(LDA)
@@ -241,26 +249,31 @@ plot(LDA)
 LDApred <- LDA %>% predict(testTransformed)
 names(LDApred)
 
+# Accuracy
 mean(LDApred$class==testTransformed$target) ## accuracy rate 0.6191652
+
+# Confusion Matrix
 confusionMatrix(LDApred$class,testTransformed$target)
 
-# Not working
+# Calculate normalized gini coefficient
 normalizedGini(as.numeric(validate$target),as.numeric(LDApred$class)) # 0.1629344
-NormalizedGini(as.numeric(LDApred$class),as.numeric(validate$target)) # 0.1629344
 
 ##### QDA #####
+set.seed(521)
 QDAmod = qda(target~.,data=underTrain,family="binomial")
 summary(QDAmod)
 
 QDApred = QDAmod %>% predict(testTransformed)
 names(QDApred)
 
+# Accuracy rate
 mean(QDApred$class==testTransformed$target) # 0.3615811
+
+# Confusion MAtrix
 table(QDApred$class,testTransformed$target)
 
 # Calculating Normalized Gini Coefficient
 normalizedGini(as.numeric(testTransformed$target),as.numeric(QDApred$class)) # 0.0162174
-NormalizedGini(as.numeric(QDApred$class),as.numeric(testTransformed$target)) # 0.0162174
 
 ##### KNN #####
 # Need a smaller training set to run KNN
@@ -270,7 +283,7 @@ divideData = createDataPartition(train$target,p = 0.2, list=FALSE)
 train3 = train[divideData,]
 validate2 = train[-divideData,]
 
-# Balance train3
+# Need to balance train3
 predictorVariables = train3[,-1]
 responseVariable = train3[,1]
 
@@ -279,6 +292,7 @@ underTrain2 = cbind(undersampledData$Y,undersampledData$X)
 names(underTrain2)[names(underTrain2) == "undersampledData$Y"] <- "target"
 head(underTrain2)
 
+# Preprocess within the train function (center & scale)
 KNNmod = train(target~., data=underTrain2,method="knn",preProcess = c("center","scale"))
 
 KNNmod$bestTune #9
@@ -287,117 +301,149 @@ plot(KNNmod) #9
 KNNpred <- predict(KNNmod, newdata=validate)
 
 confusionMatrix(KNNpred,validate$target)
+# Accuracy rate = 0.5892
 
-normalizedGini(as.numeric(validate$target),as.numeric(KNNpred)) # 0.1099644
-NormalizedGini(as.numeric(KNNpred),as.numeric(validate$target)) # 0.1099644
-
-
+# Calculate normalized gini coefficient
+normalizedGini(as.numeric(validate$target),as.numeric(KNNpred)) # 0.1065237
 
 ##### Lasso #####
+# Need to organize the features into a matrix
+# Be sure to use the balanced datasets
 x.train = model.matrix(target~.,underTrain)[,-1]
+
+# Isolate y into a variable
 y.train = underTrain$target
 
+# Repeat above prep for the test x's and y
 x.test = model.matrix(target~., validate)[,-1]
 y.test = validate$target
 
-summary(y.train)
-summary(y.test)
+summary(y.train) # y.train is balanced
+summary(y.test) # y.test is imbalanced, but that is okay because these data are not being used to train the model
 
 set.seed(521)
+# create a grid for possible lambdas
 grid = 10^seq(-2,4,length=200)
 
+# Lasso model without cross-validation
 lasso = glmnet(x.train,y.train,alpha = 1,lambda=grid,family=binomial)
+
+# Lasso model with cross-validation (to identify the best lambda)
 cv.out.lasso = cv.glmnet(x.train,y.train,alpha=1,lambda=grid,family=binomial,nfolds=12)
 bestLambda = cv.out.lasso$lambda.min
 bestLambda # 0.01
 
+# Make predictions using the best lambda value on the test set
 lasso.pred = predict(lasso,s=bestLambda,newx=x.test)
 
 target.hat = ifelse(lasso.pred>=0.5,1,0)
+
+# Confusion matrix
 table(y.test,target.hat)
+
+# Accuracy rate
 mean(y.test == target.hat) # 0.9188746
 
+# Normalized gini coefficient
 normalizedGini(as.numeric(y.test),as.numeric(target.hat)) # 0.06392911
-NormalizedGini(as.numeric(target.hat),as.numeric(y.test)) # 0.06392911
 
 ##### Ridge #####
 set.seed(521)
 power.value = seq(from=10, to=-2,length=100)
 grid = 10^power.value
 
-ridge = glmnet(x.train,y.train,alpha=0,lambda=grid,thresh=1e-12)
+# ridge = glmnet(x.train,y.train,alpha=0,lambda=grid,thres=1e-12)
 cv.out.ridge = cv.glmnet(x.train,y.train,alpha=0,lambda=grid,family=binomial,thres = 1e-12,nfolds=12)
 
 bestLambda = cv.out.ridge$lambda.min
-bestLambda # 0.02146141
+bestLambda # 0.01747528
 
+# Make predictions using best lambda on x.test
 ridgePred = predict(cv.out.ridge,s=bestLambda,newx = x.test)
-
 target.hat = ifelse(ridgePred>=0.5,1,0)
-table(y.test,target.hat)
-mean(y.test == target.hat) # 0.8891764
 
-normalizedGini(as.numeric(y.test),as.numeric(target.hat)) # 0.09039913
-NormalizedGini(as.numeric(target.hat),as.numeric(y.test)) # 0.09039913
+# Confusion matrix
+table(y.test,target.hat)
+
+# Accuracy rate
+mean(y.test == target.hat) # 0.8878715
+
+# Normalized gini coefficient
+normalizedGini(as.numeric(y.test),as.numeric(target.hat)) # 0.09127149
 
 ##### randomForest #####
-library(randomForest)
 set.seed(521)
+
+# May need to adjust memory to run the model
 memory.limit(size=56000)
+
+# start time
 start = Sys.time()
 modelrf <- randomForest(target~.,data=underTrain,ntree=200,importance=TRUE)
 end = Sys.time()
-end-start #run time 1min
+end-start #run time 1.47 mins
+
+# Make predictions
 rfpred <- predict(modelrf, newdata=validate)
+
+# Confusion matrix
 table(rfpred,validate$target) 
-mean(rfpred==validate$target) #0.5940704
-#      0      1
-#0 102643   2800
-#1  69412   3708
-normalizedGini(as.numeric(validate$target),as.numeric(rfpred)) #0.1691966
-NormalizedGini(as.numeric(rfpred),as.numeric(validate$target)) #0.1691966
+
+# Accuracy rate
+mean(rfpred==validate$target) #0.5941208
+
+# Normalized gini coefficient
+normalizedGini(as.numeric(validate$target),as.numeric(rfpred)) #0.16023
 
 ##### Bagging #####
-library(randomForest)
 set.seed(521)
 start = Sys.time()
 modelbag <- randomForest(target~.,data=underTrain,ntree=200,mtry=50,importance=TRUE)
 end = Sys.time()
-end-start #2.2min
-bagpred <- predict(modelbag,newdata=validate)
-mean(bagpred==validate$target) # 0.5934
-table(bagpred,validate$target)
-#      0      1
-#0 102323   2865
-#1  69732   3643
+end-start #2 mins
 
+# Made predictions
+bagpred <- predict(modelbag,newdata=validate)
+
+# Accuracy rate
+mean(bagpred==validate$target) # 0.5934
+
+# Confusion Matrix
+table(bagpred,validate$target)
+
+# Calculate normalized gini coefficient
 normalizedGini(as.numeric(validate$target),as.numeric(bagpred)) #0.1575381
 
 ##### Boosting #####
-library(gbm)
 set.seed(521)
 start = Sys.time()
 boost <- gbm(as.character(target)~.,underTrain, distribution="bernoulli"
              ,n.trees= 100,interaction.depth=3, shrinkage=0.02)
 end = Sys.time()
 end - start
+
+# Make predictions
 pred.gbm <- predict(boost,validate,type = "response")
 yhat <- ifelse(pred.gbm>0.5,0,1)
+
+# Accuracy rate
 mean(yhat == validate$target) #0.405
-table(yhat,validate$target) #
-#     0      1
-#0  69687   3703
-#1 102368   2805
-normalizedGini(as.numeric(validate$target),as.numeric(yhat)) #-0.1613004 
+
+# Confusion Matrix
+table(yhat,validate$target)
+
+# normalized gini coefficient
+normalizedGini(as.numeric(validate$target),as.numeric(yhat)) #-0.1603158
 
 
 ##### XGBoost #####
-  #turn train2 features into matrix
+#turn train2 features into matrix
 Train.X <- data.matrix(scale(trainTransformed[-1]))
 Train.Y <- as.numeric(trainTransformed$target)
 Train.Y[Train.Y == "1"] <- "0"
 Train.Y[Train.Y == "2"] <- "1"
 
+# turn validate features into matrix
 Test.X <- data.matrix(scale(testTransformed[-1]))
 Test.Y <- as.numeric(testTransformed$target)
 Test.Y[Test.Y == "1"] <- "0"
@@ -407,21 +453,28 @@ Train.X = xgb.DMatrix(data=Train.X, label=Train.Y)
 Test.X = xgb.DMatrix(data=Test.X, label=Test.Y)
 
 xgbmodel=xgboost( data = Train.X, #X features in matrix form
-  #label = as.numeric(Train.Y), # target is a vector
-  eta = .5, # learning rate
-  nthread = 1, # number of parallel threads
-  nrounds = 50, # number of rounds of predictions
-  objective = "binary:logistic",  # logistic
-  max.depth  = 2,  # number of splits
-  eval_metric = "error",
-  verbose = 1) # print training error
+                  #label = as.numeric(Train.Y), # target is a vector
+                  eta = .5, # learning rate
+                  nthread = 1, # number of parallel threads
+                  nrounds = 50, # number of rounds of predictions
+                  objective = "binary:logistic",  # logistic
+                  max.depth  = 2,  # number of splits
+                  eval_metric = "error",
+                  verbose = 1) # print training error
 
+# Assess importance of features
 (Importance <- xgb.importance(colnames(Train.X), model = xgbmodel))
 
+# Make predictions
 xgbpreds <- predict(xgbmodel, newdata=Test.X)
 summary(xgbpreds)
-
 predictions <- as.numeric(xgbpreds > 0.5)
-mean(predictions==Test.Y) ## accuracy rate 0.5906319
+
+# Accuracy rate
+mean(predictions==Test.Y) # 0.5906319
+
+# Confusion Matrix
 table(predictions, Test.Y)
+
+# Normalized Gini Coefficient
 normalizedGini(as.numeric(Test.Y),predictions) # 0.1836209
